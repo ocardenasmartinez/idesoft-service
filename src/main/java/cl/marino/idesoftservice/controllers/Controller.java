cd74f45ab0a6cdadef916e3946b61daf0ec169dd
Octavio <ocardenasmartinez@gmail.com>
2020-07-02 13:50:14 -0400

    add logger

--- src/main/java/cl/marino/idesoftservice/controllers/Controller.java
+++ src/main/java/cl/marino/idesoftservice/controllers/Controller.java
@@ -2,6 +2,8 @@ package cl.marino.idesoftservice.controllers;
 
 import cl.marino.idesoftservice.business.Business;
 import cl.marino.idesoftservice.entities.LocalDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,11 +17,14 @@ import java.util.List;
 @RestController
 public class Controller {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+
     @Autowired
     private Business business;
 
     @GetMapping(path = "/", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<List<LocalDTO>> getLocales(@RequestParam String comuna) {
+        LOGGER.info("Buscando locales de: " + comuna);
         return new ResponseEntity<List<LocalDTO>>(business.getLocales(comuna), HttpStatus.OK);
     }
 
--- src/main/java/cl/marino/idesoftservice/dao/DAOImpl.java
+++ src/main/java/cl/marino/idesoftservice/dao/DAOImpl.java
@@ -1,31 +1,44 @@
 package cl.marino.idesoftservice.dao;
 
+import cl.marino.idesoftservice.controllers.Controller;
 import cl.marino.idesoftservice.entities.LocalDTO;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DAOImpl implements DAO {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DAOImpl.class);
+
     @Getter
     @Setter
-    private static List<LocalDTO> listaLocales;
+    private static List<LocalDTO> listaLocales = new ArrayList<>();
 
     public DAOImpl() {
-        cargaLocales();
+        try {
+            cargaLocales();
+        } catch (Exception e) {
+            LOGGER.error("Hubo un error buscando locales");
+        }
     }
 
     //@Value("${locales-service-url}")
     private String localesURL = "https://farmanet.minsal.cl/maps/index.php/ws/getLocalesRegion?id_region=7";
 
-    public void cargaLocales() {
+    public void cargaLocales() throws Exception {
+        LOGGER.info("Buscando locales...");
         ResponseEntity<String> responseEntity = new RestTemplate().getForEntity(localesURL, String.class);
+        LOGGER.info("Locales recibidos: ");
+        LOGGER.info(responseEntity.getBody());
         listaLocales = new Gson().fromJson(responseEntity.getBody(), new TypeToken<List<LocalDTO>>(){}.getType());
     }
 
